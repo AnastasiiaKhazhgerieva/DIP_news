eeb534897010ad24e2afca13d013e37a5dd95f8e
AnastasiiaKhazhgerieva <raraavisjuncture2@gmail.com>
2025-08-07 10:29:28 +0300

    Update dip_news.py

--- dip_news.py
+++ dip_news.py
@@ -1053,11 +1053,11 @@ def prioritise(section):
     save_to_drive(file_name, filtered_list, folder_id, file_format="json")
     print(f"✅ prioritise({section}) — сохранён корректный JSON.")
 
-prioritise("world")
-time.sleep(60)
-prioritise("rus")
-time.sleep(60)
-prioritise("prices")
+#prioritise("world")
+#time.sleep(60)
+#prioritise("rus")
+#time.sleep(60)
+#prioritise("prices")
 
 def design(section):
     # Получаем JSON с отфильтрованными новостями
@@ -1088,11 +1088,11 @@ def design(section):
     file_name_txt = f"{section}.txt"
     save_to_drive(file_name_txt, response.text, "1BwBFMln6HcGUfBFN4-UlNueOTKUehiRe", file_format="txt")
 
-#design("world")
-#time.sleep(60)
-#design("rus")
-#time.sleep(60)
-#design("prices")
+design("world")
+time.sleep(60)
+design("rus")
+time.sleep(60)
+design("prices")
 #telegram_lists()
 
 def read_top_urls(section, max_chars=1500):
