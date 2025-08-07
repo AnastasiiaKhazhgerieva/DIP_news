265fc71d252ec703eb66157d6aabbe4ca5c995d5
AnastasiiaKhazhgerieva <raraavisjuncture2@gmail.com>
2025-08-07 10:23:59 +0300

    Update dip_news.py

--- dip_news.py
+++ dip_news.py
@@ -990,11 +990,11 @@ def create_news_lists(section):
     print(f"✅ create_news_lists({section}) — успешно обработан и сохранён файл.")
 
 # Kommersant, Vedomosti, RBC, Agroinvestor, RG.ru, RIA, Autostat
-create_news_lists("world")
-time.sleep(60)
-create_news_lists("rus")
-time.sleep(60)
-create_news_lists("prices")
+#create_news_lists("world")
+#time.sleep(60)
+#create_news_lists("rus")
+#time.sleep(60)
+#create_news_lists("prices")
 
 def prioritise(section):
     file_name = f"{section}.json"
@@ -1053,11 +1053,11 @@ def prioritise(section):
     save_to_drive(file_name, filtered_list, folder_id, file_format="json")
     print(f"✅ prioritise({section}) — сохранён корректный JSON.")
 
-#prioritise("world")
-#time.sleep(60)
-#prioritise("rus")
-#time.sleep(60)
-#prioritise("prices")
+prioritise("world")
+time.sleep(60)
+prioritise("rus")
+time.sleep(60)
+prioritise("prices")
 
 def design(section):
     # Получаем JSON с отфильтрованными новостями
